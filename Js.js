31ab6e0bd8e2ee52c6648e21351b7948195a8365
bek1F <xamidullayevbehruz1@gmail.com>
2025-09-30 17:55:05 +0500

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,43 @@
+let string = prompt(`stringda biron nima yozing:`);
+let number = prompt(` numberga biron nima yozing:`);
+let boolean = prompt(`bolean uchun biron nima yozing yozing:`);
+
+confirm(`ishonchingiz komilmi? shu malumotlarga!`);
+
+alert(`string uchun ${string} shu narsa yozilgan 
+number uchun ${number} shu narsa yozilgan
+boolean uchun ${boolean} shu narsa yozilgan`);
+
+console.log(string);
+console.log(number);
+console.log(boolean);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
